cbfd6d34e4d5d5b5bb921386e869eefc717af178
natitaw <nathnaelshenkute@gmail.com>
2020-11-12 17:04:28 +0100

    Normalization and Standardization

--- Deep Learning with PyTorch/my_model.py
+++ Deep Learning with PyTorch/my_model.py
@@ -6,6 +6,10 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 import torch.optim as optim
 
+from collections import OrderedDict
+from collections import namedtuple
+import time
+
 from torch.utils.data import Dataset
 
 # Implementing the forward method
@@ -55,3 +59,116 @@ class Network(nn.Module): # extending nn.Module base class
         
         return t
 
+
+# Run Builder 
+class RunBuilder():
+    @staticmethod
+    def get_runs(params):
+        
+        Run = namedtuple('Run', params.keys())
+        
+        runs = []
+        
+        for v in product(*params.values()):
+            runs.append(Run(*v))
+        
+        return runs
+
+    
+class RunManager():
+    def __init__(self):
+        
+        # we will need to extract a class out of these epoch values
+        self.epoch_count = 0
+        self.epoch_loss = 0
+        self.epoch_num_correct = 0
+        self.epoch_start_time = None
+        
+        self.run_params = None
+        self.run_count = 0
+        self.run_data = []
+        self.run_start_time = None
+        
+        self.network = None
+        self.loader = None
+        self.tb = None
+        
+    def begin_run(self, run, network, loader):
+        
+        self.run_start_time = time.time()
+        
+        self.run_params = run
+        self.run_count += 1
+        
+        self.network = network
+        self.loader = loader
+        self.tb = SummaryWriter(comment=f'-{run}')
+        
+        images, labels = next(iter(self.loader))
+        grid = torchvision.utils.make_grid(images)
+        
+    def end_run(self):
+        self.tb.close()
+        self.epoch_count = 0
+        
+    def begin_epoch(self):
+        self.epoch_start_time = time.time()
+        
+        self.epoch_count += 1
+        self.epoch_loss = 0
+        self.epoch_num_correct = 0
+        
+    def end_epoch(self):
+        
+        epoch_duration = time.time() - self.epoch_start_time
+        run_duration = time.time() - self.run_start_time
+        
+        loss = self.epoch_loss / len(self.loader.dataset)
+        accuracy = self.epoch_num_correct / len(self.loader.dataset)
+        
+        self.tb.add_scalar('Loss', loss, self.epoch_count)
+        self.tb.add_scalar('Accuracy', accuracy, self.epoch_count)
+        
+        for name, param in self.network.named_parameters():
+            self.tb.add_histogram(name, param, self.epoch_count)
+            self.tb.add_histogram(f'{name}.grad', param.grad, self.epoch_count)
+            
+        results = OrderedDict()
+        results["run"] = self.run_count
+        results["epoch"] = self.epoch_count
+        results["loss"] = loss
+        results["accuracy"] = accuracy
+        results["epoch duration"] = epoch_duration
+        results["run duration"] = run_duration
+        
+        for k,v in self.run_params._asdict().items():
+            results[k] = v
+        
+        self.run_data.append(results)
+        
+        df = pd.DataFrame.from_dict(self.run_data, orient='columns')
+        
+        clear_output(wait=True)
+        display(df)
+        
+    def track_loss(self, loss):
+        self.epoch_loss += loss.item() * self.loader.batch_size
+        
+    def track_num_correct(self, preds, labels):
+        self.epoch_num_correct += self._get_num_correct(preds, labels)
+        
+    @torch.no_grad()
+    def _get_num_correct(self, preds, labels):
+        # underscore indicates that this method should not really 
+        # be used outside this class
+        return preds.argmax(dim=1).eq(labels).sum().item()
+    
+    def save(self,fileName):
+        
+        pd.DataFrame.from_dict(
+            self.run_data,
+            orient='columns',
+        ).to_csv(f'{fileName}.csv')
+        
+        with open(f'{fileName}.json', 'w', encoding='utf-8') as f:
+            json.dump(self.run_data, f, ensure_ascii=False, indent=4)
